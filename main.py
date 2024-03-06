6a04c958cc2f11c05536a5070c6f0d79af5f8a0f
Chathet <w.bochenski.05@gmail.com>
2024-03-06 19:25:44 +0100

    essa

---
+++ main.py
@@ -0,0 +1,18 @@
+n = int(input())
+l = []
+
+for i in range(n):
+    l.append(int(input()))
+
+l2 = l.sort()
+max = 0
+print(2[0])
+for i in range(1):
+    for j in l:
+        if j >= i:
+            a += 1
+        else:
+            a=0
+        if a * i> max: max = a*i
+
+print(max)
